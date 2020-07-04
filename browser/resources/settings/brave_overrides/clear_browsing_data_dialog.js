2c8f1b6a51914873dadafd6af626e23903b7bd39
NejcZdovc <nejc.zdovc@3zsistemi.si>
2020-07-04 07:01:28 +0200

    Adds UI to clear browsing data flow
    https://github.com/brave/brave-browser/issues/926

--- browser/resources/settings/brave_overrides/clear_browsing_data_dialog.js
+++ browser/resources/settings/brave_overrides/clear_browsing_data_dialog.js
@@ -4,10 +4,17 @@
 // you can obtain one at http://mozilla.org/MPL/2.0/.
 
 import {BraveClearBrowsingDataOnExitBehavior} from '../brave_clear_browsing_data_dialog/brave_clear_browsing_data_dialog_behavior.js'
-import {RegisterPolymerComponentBehaviors} from 'chrome://brave-resources/polymer_overriding.js'
+import {RegisterPolymerComponentBehaviors, RegisterPolymerTemplateModifications} from 'chrome://brave-resources/polymer_overriding.js'
 
 RegisterPolymerComponentBehaviors({
   'settings-clear-browsing-data-dialog': [
     BraveClearBrowsingDataOnExitBehavior
   ]
+})
+
+RegisterPolymerTemplateModifications({
+  'settings-clear-browsing-data-dialog': (templateContent) => {
+    const body = templateContent.querySelector('[slot="body"]')
+    body.innerHTML = body.innerHTML + '<div>To reset rewards data please click <a href="chrome://rewards/#manage-wallet">here</a>.</div>'
+  }
 })
